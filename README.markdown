1991ae42b2498c48977fe6f7638d0d8534143c8a
jjohnson55555 <60401024+jjohnson55555@users.noreply.github.com>
2020-03-16 18:15:04 -0500

    Add files via upload

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# dev07-build-js-func
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/barrycumbie/pen/oVpRwO](https://codepen.io/barrycumbie/pen/oVpRwO).
+
+ 
---
+++ dist/script.js
@@ -0,0 +1,41 @@
+// Create variables for the welcome message
+var greeting = "[not initialized]";
+var name = "[not initialized]";
+var message = "[not initialized]";
+// Concatenate the three variables above to create the welcome message
+var welcome;
+var sign = "[not initialized]";
+var tiles, subTotal, shipping, grandTotal;
+
+function calcSign() {
+  tiles = sign.length;
+  subTotal = tiles * 5;
+  shipping = 7;
+  grandTotal = subTotal + shipping;
+}
+
+function initiateVars() {
+  greeting = "Howdy";
+  name = "Barry";
+  message = "Plz check your order:";
+  sign = "javascript";
+  welcome = greeting + "! " + name + ", " + message;
+  calcSign();
+}
+
+function setTextContentById(getId, setMsg) {
+  // Get the element that has an id of greeting
+  var el = document.getElementById(getId);
+  // Replace the content of that element with the personalized welcome message
+  el.textContent = setMsg;
+}
+
+(function() {
+  initiateVars();
+  setTextContentById("greeting", welcome);
+  setTextContentById("userSign", sign);
+  setTextContentById("tiles", tiles);
+  setTextContentById("subTotal", "$" + subTotal);
+  setTextContentById("shipping", "$" + shipping);
+  setTextContentById("grandTotal", "$" + grandTotal);
+});
